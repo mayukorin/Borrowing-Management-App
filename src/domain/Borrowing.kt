eef94720941c49b3f012a33af913e5211e335670
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2025-10-12 15:30:29 +0900

    Borrowing.kt を作成

---
+++ src/domain/Borrowing.kt
@@ -0,0 +1,51 @@
+package domain
+
+import com.github.michaelbull.result.*
+
+class Borrowing private constructor(
+    val id: BorrowingId,
+    val employeeId: EmployeeId,
+    val equipmentId: EquipmentId,
+    val period: Period,
+    val isReturned: Boolean
+) {
+    companion object {
+        fun create(
+            id: BorrowingId,
+            employeeId: EmployeeId,
+            equipmentId: EquipmentId,
+            period: Period
+        ): Borrowing {
+            return Borrowing(
+                id = id,
+                employeeId = employeeId,
+                equipmentId = equipmentId,
+                period = period,
+                isReturned = false
+            )
+        }
+    }
+
+    fun return(): Result<Borrowing, BorrowingError> {
+        if (isReturned) {
+            return Err(BorrowingError.AlreadyReturned)
+        }
+        return Ok(
+            Borrowing(
+                id = id,
+                employeeId = employeeId,
+                equipmentId = equipmentId,
+                period = period,
+                isReturned = true
+            )
+        )
+    }
+
+    override fun toString(): String {
+        return "Borrowing(id=$id, employeeId=$employeeId, equipmentId=$equipmentId, period=$period, isReturned=$isReturned)"
+    }
+}
+
+sealed class BorrowingError {
+    data object AlreadyReturned : BorrowingError()
+}
